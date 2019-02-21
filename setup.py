75e20299934f0a24dbe80adb1a45b321a3a6d172
ArthMx <arthur.moraux@gmail.com>
2019-02-21 23:03:46 +0100

    update version id

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='torchtrainer',
-    version='0.0.2',
+    version='0.0.3',
     author='Arthur Moraux',
     author_email='arthur.moraux@gmail.com',
     description='A package for training pytorch models.',
